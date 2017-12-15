89f292f3a13519123978649d2121e4f7e2decf1e
Joao Paulo <jp.cedeira@gmail.com>
2017-12-15 12:46:03 -0200

    push notification

--- TaxiManager/AppDelegate.swift
+++ TaxiManager/AppDelegate.swift
@@ -11,6 +11,7 @@ import UIKit
 import GoogleMaps
 import GooglePlaces
 import Firebase
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -22,7 +23,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         FirebaseApp.configure()
-    
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (success, error) in
+            if (error == nil){
+                print("Successful Authorization")
+            }
+        }
+    application.registerForRemoteNotifications()
+        NotificationCenter.default.addObserver(self, selector: #selector(self.refreshToken(notification:)), name: NSNotification.Name.InstanceIDTokenRefresh, object: nil)
+        
         GMSServices.provideAPIKey("AIzaSyDOebYw-atZqhQxDKX-D5Ps5Dje0f29RSo")
         GMSPlacesClient.provideAPIKey("AIzaSyDOebYw-atZqhQxDKX-D5Ps5Dje0f29RSo")
         
@@ -65,6 +73,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
+    @objc func refreshToken(notification : NSNotification){
+        let refreshToken = InstanceID.instanceID().token()
+        print("--------\(refreshToken)--------")
+        FBHandler()
+    }
+    func FBHandler (){
+        Messaging.messaging().shouldEstablishDirectChannel = true
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -74,6 +90,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        Messaging.messaging().shouldEstablishDirectChannel = false
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -82,6 +99,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        FBHandler()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
